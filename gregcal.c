6c21902685761473b3e483be1d8309e94f2598d7
Divyanshu Kumar <56974172+divyanshukumarpcm@users.noreply.github.com>
2020-09-18 22:27:12 +0530

    Add files via upload
    V2 with added features: can calculate number of days passed since beginning of AD 0000 till the entered date in YYYY-MM-DD format

--- gregcal.c
+++ gregcal.c
@@ -1,13 +1,18 @@
 #define DAYS_IN_MONTHS {31,28,31,30,31,30,31,31,30,31,30,31}
 #include<stdio.h>
-int isleap(char*);
+int isleap(int);
 int main(){
-	printf("Enter date in YYYY-MM-DD format with the hifens. The code will tell you how many days have been passed since beginning of that year.\n THis requires a  C compiler");
+	printf("Enter date in YYYY-MM-DD format with the hifens. The code will tell you how many days have been passed since beginning of that year. and since 0000 AD: \n ");
 	char dat[10];
 	int days[12]=DAYS_IN_MONTHS;
 	
 	scanf("%s",dat);
 	//printf("%s \n",dat);
+	int year=0;
+	year+=(*(dat+0)-48)*1000;
+	year+=(*(dat+1)-48)*100;
+	year+=(*(dat+2)-48)*10;
+	year+=(*(dat+3)-48);
 	int months=0;
 	months+=(dat[5]-48)*10;
 	months+=(dat[6]-48);
@@ -18,27 +23,32 @@ int main(){
 	//printf("%d \n");
 	char * s=dat;
 	int total=days_open;
-	if(months>2) total+=isleap(s);
+	if(months>2) total+=isleap(year);
 	for(int i=1;i<months;++i){
 		total+=days[i];
 	}
 	
-	printf("\n %d \n",total);
+	printf("\n DAys since beginning of the year: %d \n",total);
 	
+	
+	int godtotal=0;
+	for(int i=0;i<year;++i){
+		godtotal+=365+isleap(i);
+	}
+	godtotal+=total;
+	
+	printf(" Days since beginning of AD 0000: %d \n",godtotal);
+	printf("PLease note that this code consider 0000 th year as non-leap year. PLease see wikipedia about leap year to get detailed knowledge !!!");
 	return 0;
 }
 
-int isleap(char *dat){
-	int year=0;
-	year+=(*(dat+0)-48)*1000;
-	year+=(*(dat+1)-48)*100;
-	year+=(*(dat+2)-48)*10;
-	year+=(*(dat+3)-48);
-	//printf("%d",year);
-	if(year%4==0){
+int isleap(int year){
+	if(year==0000) return 0;
+	else if(year%4==0){
 		if(year%100==0 && year%400!=0) return 0;
 		else return 1;
 	}
+	
 	else return 0;
 		
 }
